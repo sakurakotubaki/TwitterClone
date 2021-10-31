5e05ca84abb502c161a2bc6cd6eeb094fe300c04
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-10-31 15:28:13 +0900

    borderまで作成

---
+++ lib/model/post.dart
@@ -0,0 +1,8 @@
+class Post {
+  String id;
+  String content;
+  String postActionId;
+  DateTime? createdTime;
+
+  Post({this.id = '', this.content = '', this.postActionId = '', this.createdTime});
+}
